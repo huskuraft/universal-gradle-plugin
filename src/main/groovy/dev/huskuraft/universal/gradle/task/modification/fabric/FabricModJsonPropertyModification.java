7f218a0d7b7b40d9d1e00f8cfc7cee4fb5d44e73
huskcasaca <100605532+huskcasaca@users.noreply.github.com>
2025-11-28 02:23:33 +0800

    feat: Fabric json entry order

--- src/main/groovy/dev/huskuraft/universal/gradle/task/modification/fabric/FabricModJsonPropertyModification.java
+++ src/main/groovy/dev/huskuraft/universal/gradle/task/modification/fabric/FabricModJsonPropertyModification.java
@@ -34,60 +34,101 @@ public class FabricModJsonPropertyModification extends JsonModification {
 
     @Override
     protected void modifyJson(JsonObject jsonObject) {
-        // Update basic mod information
-        jsonObject.addProperty("id", mod.getId());
-        jsonObject.addProperty("name", mod.getName());
-        jsonObject.addProperty("version", mod.getVersion());
-        jsonObject.addProperty("description", mod.getDescription());
-
-        // Update authors
+        // Create a new ordered JsonObject to control field order
+        var ordered = new JsonObject();
+        
+        // 1. Copy schemaVersion first if it exists
+        if (jsonObject.has("schemaVersion")) {
+            ordered.add("schemaVersion", jsonObject.get("schemaVersion"));
+        }
+        
+        // 2. Add basic mod information
+        ordered.addProperty("id", mod.getId());
+        ordered.addProperty("version", mod.getVersion());
+        ordered.addProperty("name", mod.getName());
+        ordered.addProperty("description", mod.getDescription());
+        
+        // 3. Add authors
         var authorsArray = new JsonArray();
         mod.getAuthors().forEach(authorsArray::add);
-        jsonObject.add("authors", authorsArray);
-
-        // Update contact information
-        var contactObject = jsonObject.get("contact").getAsJsonObject();
+        ordered.add("authors", authorsArray);
+        
+        // 4. Copy contributors if exists
+        if (jsonObject.has("contributors")) {
+            ordered.add("contributors", jsonObject.get("contributors"));
+        }
+        
+        // 5. Add contact information
+        var contactObject = new JsonObject();
+        if (jsonObject.has("contact")) {
+            contactObject = jsonObject.get("contact").getAsJsonObject();
+        }
         contactObject.addProperty("homepage", mod.getPrimaryUrl().toString());
         contactObject.addProperty("issues", mod.getSupportUrl().toString());
         contactObject.addProperty("sources", mod.getSourcesUrl().toString());
-        jsonObject.add("contact", contactObject);
-
-        // Update license
-        jsonObject.addProperty("license", mod.getLicense());
-
-        // Update icon
-        jsonObject.addProperty("icon", "assets/" + mod.getId() + "/icon.png");
-
-        // Update environment
+        ordered.add("contact", contactObject);
+        
+        // 6. Add license
+        ordered.addProperty("license", mod.getLicense());
+        
+        // 7. Add icon (RIGHT AFTER license - this is the key ordering)
+        ordered.addProperty("icon", "assets/" + mod.getId() + "/icon.png");
+        
+        // 8. Add environment
         switch (mod.getEnvironment()) {
             case BOTH:
-                jsonObject.addProperty("environment", "*");
+                ordered.addProperty("environment", "*");
                 break;
             case CLIENT:
-                jsonObject.addProperty("environment", "client");
+                ordered.addProperty("environment", "client");
                 break;
             case SERVER:
-                jsonObject.addProperty("environment", "server");
+                ordered.addProperty("environment", "server");
                 break;
         }
-
-        // Update entrypoints
-        var entrypointsObject = jsonObject.get("entrypoints").getAsJsonObject();
-
+        
+        // 9. Add entrypoints
+        var entrypointsObject = new JsonObject();
+        if (jsonObject.has("entrypoints")) {
+            entrypointsObject = jsonObject.get("entrypoints").getAsJsonObject();
+        }
+        
         var mainEntrypointArray = new JsonArray();
         mainEntrypointArray.add(mod.getGroupId() + ".fabric.platform.FabricInitializer");
         entrypointsObject.add("main", mainEntrypointArray);
-
+        
         var clientEntrypointArray = new JsonArray();
         clientEntrypointArray.add(mod.getGroupId() + ".fabric.platform.FabricClientInitializer");
         entrypointsObject.add("client", clientEntrypointArray);
-
-        // Update mixins
+        ordered.add("entrypoints", entrypointsObject);
+        
+        // 10. Copy jars if exists
+        if (jsonObject.has("jars")) {
+            ordered.add("jars", jsonObject.get("jars"));
+        }
+        
+        // 11. Add mixins
         var mixinsArray = new JsonArray();
         mixinsArray.add(mod.getId() + ".mixins.json");
-        jsonObject.add("mixins", mixinsArray);
-
-        // Update access widener
-        jsonObject.addProperty("accessWidener", mod.getId() + ".accesswidener");
+        ordered.add("mixins", mixinsArray);
+        
+        // 12. Add access widener
+        ordered.addProperty("accessWidener", mod.getId() + ".accesswidener");
+        
+        // 13. Copy any remaining fields (depends, suggests, breaks, conflicts, custom, etc.)
+        for (var entry : jsonObject.entrySet()) {
+            if (!ordered.has(entry.getKey())) {
+                ordered.add(entry.getKey(), entry.getValue());
+            }
+        }
+        
+        // Replace the original object's contents with the ordered version
+        var keys = jsonObject.keySet().toArray(new String[0]);
+        for (String key : keys) {
+            jsonObject.remove(key);
+        }
+        for (var entry : ordered.entrySet()) {
+            jsonObject.add(entry.getKey(), entry.getValue());
+        }
     }
 }
